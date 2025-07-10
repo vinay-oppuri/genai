1346b2d17bd3aca10d57bbcd11a1b2a7aa73b5f0
Vinay Reddy <oppurivinay25@gmail.com>
2025-07-10 19:54:55 +0530

    agent page

--- modules/agents/server/procedures.ts
+++ modules/agents/server/procedures.ts
@@ -7,18 +7,28 @@ import { agentsInsertSchema } from "../schemas";
 
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
-        .query(async ({ input }) => {
+        .query(async ({ input, ctx }) => {
             const [existingAgent] = await db
                 .select({
                     meetingCount: sql<number>`5`,
                     ...getTableColumns(agents)
                 })
                 .from(agents)
-                .where(eq(agents.id, input.id))
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+
+            if (!existingAgent) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+            }
 
             return existingAgent
         }),
---
+++ modules/agents/ui/components/agent-id-view-header.tsx
@@ -0,0 +1,72 @@
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem
+} from "@/components/ui/dropdown-menu"
+
+import { ChevronRightIcon, TrashIcon, PencilIcon, MoreVerticalIcon, Edit } from "lucide-react"
+import Link from "next/link"
+
+interface Props {
+    agentId: string
+    agentName: string
+    onEdit: () => void
+    onRemove: () => void
+}
+
+export const AgentIdViewHeader = ({
+    agentId,
+    agentName,
+    onEdit,
+    onRemove
+}: Props) => {
+
+    return (
+        <div className="flex items-center justify-between">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild className="font-medium text-left">
+                            <Link href="/agents">
+                                My Agents
+                            </Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+                        <ChevronRightIcon />
+                    </BreadcrumbSeparator>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild className="font-medium text-left text-foreground">
+                            <Link href={`/agents/${agentId}`}>
+                                {agentName}
+                            </Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            {/* without modal={false}, the dialog this opens causes website to stuck */}
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger>
+                    <MoreVerticalIcon className="size-5 text-muted-foreground"/>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem onClick={onEdit}>
+                        <PencilIcon className="size-4 text-black"/> Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={onRemove}>
+                        <TrashIcon className="size-4 text-black"/> Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    )
+}
